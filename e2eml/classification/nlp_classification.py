Optimized transformers loop and fixed wrong meaning calculation.

e2eml/classification/nlp_classification.py
@@ -465,7 +465,7 @@ class NlpModel(ClassificationModels, BERTDataSet):
                             "bestscore": bestscore
                         }
                         torch.save(state, "model" + str(fold) + ".pth")
-                    elif bestscore > valscore:
+                    elif bestscore < valscore:
                         bestscore = valscore
                         print("found better point")
                         state = {

e2eml/regression/nlp_regression.py
@@ -494,7 +494,7 @@ class NlpModel(RegressionModels, BERTDataSet):
                             "bestscore": bestscore
                         }
                         torch.save(state, "model" + str(fold) + ".pth")
-                    elif bestscore < valscore:
+                    elif bestscore > valscore:
                         bestscore = valscore
                         print("found better point")
                         state = {